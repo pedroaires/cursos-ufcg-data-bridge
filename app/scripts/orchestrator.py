b98187e76655c9940176ec3e52678e73d1082a7d
pedroaires <pedro.aires.cabral@gmail.com>
2024-09-10 02:17:49 -0300

    Add: Adicionado feature para processar por batches

--- app/scripts/orchestrator.py
+++ app/scripts/orchestrator.py
@@ -3,7 +3,7 @@ import redis
 import os
 import subprocess
 import time 
-from celery import chain, group, shared_task
+from celery import chain, group, chord, shared_task
 from config.db_config import reset_database, backup_database
 from core.tasks import raise_table_simple, raise_table_with_result, app
 
@@ -34,21 +34,37 @@ class RedisLock:
             return False
     
 
-def build_workflow():
+@shared_task(ignore_result=True)
+def finish_batched_workflow(prev_task_result=None):
+    logger.info("Todos os Batches finalizados!")
+
+def build_batches(cursos_data, batch_size=10):
+    batch_workflows = []
+    for batch in batch_data(cursos_data, batch_size):
+        batch_workflow = batched_workflow(batch)
+        batch_workflows.append(batch_workflow)
+    return batch_workflows
+
+    
+def batched_workflow(cursos_data):
     workflow = chain(
-        raise_table_simple.s("Cursos"),
         group(
-            raise_table_with_result.s("Alunos"),
-            raise_table_with_result.s("Curriculos"),
+            raise_table_with_result.si(cursos_data, "Alunos"),
+            raise_table_with_result.si(cursos_data, "Curriculos"),
         ),
         raise_table_with_result.s("Disciplinas"),
         group(
             raise_table_with_result.s("Historico"),
             raise_table_with_result.s("Prerequisitos"),
-        ),
-        finish_task.s()
+        )
     )
     return workflow
+
+    
+
+def batch_data(data, batch_size=10):
+    for i in range(0, len(data), batch_size):
+        yield data[i:i+batch_size]
 
 @shared_task(ignore_result=True)
 def finish_task(prev_task_result=None):
@@ -56,7 +72,7 @@ def finish_task(prev_task_result=None):
     RedisLock(redis_client, lock_name).release()
     print("Workflow finalizado!")
 
-@shared_task
+@shared_task(ignore_result=True)
 def orchestrate_tasks():
     lock_name = "orchestrate_tasks_lock"
     lock = RedisLock(redis_client, lock_name)
@@ -76,8 +92,10 @@ def orchestrate_tasks():
             logger.info(msg="BD resetado!")
 
             logger.info(msg="Iniciando workflow...")
-            workflow = build_workflow()
-            workflow.apply_async()
+            cursos_data = raise_table_simple("Cursos")
+            batch_workflows = build_batches(cursos_data)
+            chord(batch_workflows)(finish_task.s())
+            logger.info(msg="Workflow iniciado!")
         except Exception as e:
             logger.error(f"Erro em orchestrate_tasks: {e}")
             lock.release()
@@ -86,18 +104,19 @@ def orchestrate_tasks():
         return None
 
 if __name__ == '__main__':
-    worker = subprocess.Popen(['celery', '-A', 'core.tasks', 'worker', '--loglevel=info'])
-    time.sleep(5)
-    result = app.send_task('scripts.orchestrator.orchestrate_tasks')
-    inspector = app.control.inspect()
+    # worker = subprocess.Popen(['celery', '-A', 'core.tasks', 'worker', '--loglevel=info', '-n', 'worker_from_script', '--concurrency=1', '--max-tasks-per-child=1'])
+    # time.sleep(5)
+    # result = app.send_task('scripts.orchestrator.orchestrate_tasks')
+    # inspector = app.control.inspect()
     
-    while True:
-        active_tasks = inspector.active()
+    # while True:
+    #     active_tasks = inspector.active()
         
-        # Check if there are any active tasks
-        if not active_tasks or all(len(tasks) == 0 for tasks in active_tasks.values()):
-            break
-        
-        # Sleep for a bit before checking again
-        time.sleep(5)
-    worker.terminate()
+    #     # Check if there are any active tasks
+    #     if len(active_tasks.items()) > 0:
+    #         logger.info(f"Tasks ativas: {active_tasks}")
+    #         time.sleep(30)
+    #     else:
+    #         logger.info("Tarefas finalizadas")
+    #         worker.terminate()
+    app.send_task('scripts.orchestrator.orchestrate_tasks')
